ee39152e391b00fd6387450758d15162d3771085
jimbo23 <kiefersoon@gmail.com>
2023-11-04 18:28:03 +0800

    Fix typo

--- src/components/current-weather.tsx
+++ src/components/current-weather.tsx
@@ -1,43 +1,44 @@
+import { SerializedError } from '@reduxjs/toolkit';
+import { FetchBaseQueryError } from '@reduxjs/toolkit/query';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import { useGetWeatherQuery } from '../api/weather-api';
 import { getFormattedDateTimeString } from '../utils/get-formatted-date-time-string';
-import { FetchBaseQueryError } from '@reduxjs/toolkit/query';
-import { SerializedError } from '@reduxjs/toolkit';
 
 export const CurrentWeather = () => {
   const [searchParams] = useSearchParams();
   const currentSearch = searchParams.get('q');
-  const { data, isLoading, isFetching, isError, error, currentData } =
-    useGetWeatherQuery(currentSearch || 'singapore');
+  const { data, isLoading, isFetching, isError, error } = useGetWeatherQuery(
+    currentSearch || 'singapore'
+  );
 
   if (isLoading || isFetching) return <CurrentWeather.Loading />;
   if (isError) return <CurrentWeather.Error error={error} />;
 
-  if (currentData)
+  if (data)
     return (
       <div className="flex justify-between mb-3">
         <div className="flex flex-col flex-[0.5]">
           <span className="font-semibold text-sm mb-1">Today's weather</span>
           <span className="text-5xl md:text-6xl mb-1 text-purple-700 font-bold tracking-tighter">
-            {currentData.main.temp.toFixed(0)}&#8451;
+            {data.main.temp.toFixed(0)}&#8451;
           </span>
           <span className="text-sm font-semibold mb-1">
-            H:{currentData.main.temp_max.toFixed(0)}&#8451; L:
-            {currentData.main.temp_min.toFixed(0)}&#8451;
+            H:{data.main.temp_max.toFixed(0)}&#8451; L:
+            {data.main.temp_min.toFixed(0)}&#8451;
           </span>
           <span className="text-sm text-gray-800/80 font-semibold">
-            {currentData.name}, {currentData.sys.country}
+            {data.name}, {data.sys.country}
           </span>
         </div>
         <div className="flex flex-col-reverse text-end self-end md:flex-row md:justify-between flex-[0.5] md:flex-1">
           <span className="text-sm text-gray-800/50 font-medium">
-            {getFormattedDateTimeString(currentData.dt * 1000)}
+            {getFormattedDateTimeString(data.dt * 1000)}
           </span>
           <span className="text-sm text-gray-800/50 font-medium">
-            Humidity: {currentData.main.humidity}%
+            Humidity: {data.main.humidity}%
           </span>
           <span className="text-sm text-gray-800/50 font-medium">
-            {currentData.weather[0].main}
+            {data.weather[0].main}
           </span>
         </div>
       </div>
